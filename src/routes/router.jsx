f967679096b60e6af698cb184ce5c3d5403c11f3
Abtahi chowdhury <abtahejisan@gmail.com>
2026-01-29 12:04:03 +0600

    akj nai

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -7,6 +7,7 @@ import Shops from "../pages/Shops";
 import Blog_List from "../pages/Blog/Blog_List";
 import Wishlist from "../pages/WishListSctions/Wishlist";
 import Checkout from "../pages/Checkout/Checkout";
+import Single_Blog from "../pages/Blog/Single_Blog";
 
 export const router = createBrowserRouter([
   {
@@ -36,6 +37,10 @@ export const router = createBrowserRouter([
       {
         path: "/blog",
         element: <Blog_List />,
+      },
+      {
+        path:"/singleblog/:id",
+        element:<Single_Blog/>
       },
       {
         path: "/checkout",
