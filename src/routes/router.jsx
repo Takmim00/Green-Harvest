f262d7c3f8206f2b94c188dce4fded343a2f8d19
Abtahi chowdhury <abtahejisan@gmail.com>
2026-01-29 11:56:12 +0600

    bug fix

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,13 +4,9 @@ import Home from "../pages/Home";
 import ProductDetails from "../pages/ProductDetails";
 import ShoppingCart from "../pages/ShopingCart/ShoppingCart";
 import Shops from "../pages/Shops";
-<<<<<<< HEAD
-import Wishlist from "../pages/PaymentSctions/Wishlist";
 import Blog_List from "../pages/Blog/Blog_List";
-=======
 import Wishlist from "../pages/WishListSctions/Wishlist";
 import Checkout from "../pages/Checkout/Checkout";
->>>>>>> 4b2cb5a2b410e4b018d1a40cf263a8947db04c3b
 
 export const router = createBrowserRouter([
   {
@@ -34,17 +30,16 @@ export const router = createBrowserRouter([
         element: <Wishlist />,
       },
       {
-<<<<<<< HEAD
-        path:"/blog",
-        element:<Blog_List/>
-=======
         path: "/shoppingCart",
         element: <ShoppingCart />,
       },
       {
+        path: "/blog",
+        element: <Blog_List />,
+      },
+      {
         path: "/checkout",
         element: <Checkout />,
->>>>>>> 4b2cb5a2b410e4b018d1a40cf263a8947db04c3b
       },
     ],
   },
