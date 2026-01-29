f8e429d0151e28af8ac092ccbbf6411d01288d59
Takmim00 <takmimm@gmail.com>
2026-01-29 19:44:48 +0600

    breadcrumd added

---
+++ src/shared/Breadcrumb.jsx
@@ -0,0 +1,44 @@
+import { ChevronRight, House } from "lucide-react";
+import { Link, useLocation } from "react-router";
+
+const routeNames = {
+  shop: "Shop",
+  blog: "Blog",
+  about: "About Us",
+  contact: "Contact Us",
+  wishlist: "Wishlist",
+  shoppingCart: "Cart",
+};
+
+export default function Breadcrumb() {
+  const location = useLocation();
+
+  // 🔴 Home page হলে breadcrumb hide
+  if (location.pathname === "/") return null;
+
+  const pathParts = location.pathname.split("/").filter(Boolean);
+  const currentPage =
+    routeNames[pathParts[0]] ||
+    pathParts[0]?.charAt(0).toUpperCase() + pathParts[0]?.slice(1);
+
+  return (
+    <div
+      className="bg-gray-50 py-6"
+      style={{ backgroundImage: "url(/Breadcrumbs.png)" }}
+    >
+      <div className="lg:max-w-7xl max-w-11/12 mx-auto px-4">
+        <div className="flex items-center gap-2 text-base text-gray-500">
+          <Link to="/" className="hover:text-green-600">
+             <House />
+          </Link>
+
+          <ChevronRight size={16} />
+
+          <span className="text-green-600 font-medium">
+            {currentPage}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
